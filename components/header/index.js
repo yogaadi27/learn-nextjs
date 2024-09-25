b401752e29c170c496a8ef56715219d220769581
yogaadi27m <kuloyoga9@gmail.com>
2024-09-26 00:12:15 +0700

    tugas hari-3

---
+++ components/header/index.js
@@ -0,0 +1,9 @@
+import Menu from "../menu"
+
+export default function Header() {
+    return <>
+                <div>
+                    <Menu/>
+                </div>
+            </>
+}
---
+++ components/menu/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Menu() {
+    return (
+        <>
+                <Link href="/">Home</Link>
+                <Link href="/profile">Profile</Link>
+
+        </>
+            
+    )
+}
